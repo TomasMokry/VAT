d75851c5661c319e5f991f3eec6ba8683d708491
tomas_mokry <tomas.mokry@gmail.com>
2023-03-27 10:05:54 +0200

    State class completed

--- src/com/engeto/vat/State.java
+++ src/com/engeto/vat/State.java
@@ -1,11 +1,67 @@
 package com.engeto.vat;
 
 public class State {
-    //TODO
-    //zkratka státu (například AT)
-    //název státu (Austria)
-    //plná sazba daně z přidané hodnoty v procentech (20)
-    //snížená sazba daně z přidané hodnoty v procentech (10)
-    //informace o tom, jestli země používá speciální sazbu DPH pro některé produkty (true/false)
-    //Jednotlivé hodnoty jsou odděleny vždy tabulátorem (v Javě zapíšeme tabulátor jako "\t")
+    private String stateID;
+    private String stateName;
+    private double regularTax;
+    private double reducedTax;
+    private boolean hasSpecialTax;
+
+    public State(String stateID, String stateName, double regularTax, double reducedTax, boolean hasSpecialTax) {
+        this.stateID = stateID;
+        this.stateName = stateName;
+        this.regularTax = regularTax;
+        this.reducedTax = reducedTax;
+        this.hasSpecialTax = hasSpecialTax;
+    }
+
+    public String getStateID() {
+        return stateID;
+    }
+
+    public void setStateID(String stateID) {
+        this.stateID = stateID;
+    }
+
+    public String getStateName() {
+        return stateName;
+    }
+
+    public void setStateName(String stateName) {
+        this.stateName = stateName;
+    }
+
+    public double getRegularTax() {
+        return regularTax;
+    }
+
+    public void setRegularTax(double regularTax) {
+        this.regularTax = regularTax;
+    }
+
+    public double getReducedTax() {
+        return reducedTax;
+    }
+
+    public void setReducedTax(double reducedTax) {
+        this.reducedTax = reducedTax;
+    }
+
+    public boolean isHasSpecialTax() {
+        return hasSpecialTax;
+    }
+
+    public void setHasSpecialTax(boolean hasSpecialTax) {
+        this.hasSpecialTax = hasSpecialTax;
+    }
+
+    @Override
+    public String toString() {
+        return this.stateName
+                + " ("
+                + this.stateID
+                + "): "
+                + this.regularTax
+                + " %";
+    }
 }
